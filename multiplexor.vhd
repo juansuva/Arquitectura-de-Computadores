e08778a7f58e1becb2ebffe51cafde642629bfbb
alejocq <alejandre@utp.edu.co>
2016-10-05 22:51:34 -0500

    Add files via upload

--- multiplexor.vhd
+++ multiplexor.vhd
@@ -1,11 +1,12 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
---el selector es el bit 13 que indica si es un inmmediato o un registro entity MUX is
-		  PORT(  a      : IN std_logic_vector(31 DOWNTO 0); 
+entity MUX is
+		  PORT(  
+				 a      : IN std_logic_vector(31 DOWNTO 0); 
              b      : IN std_logic_vector(31 DOWNTO 0); 
              opt    : IN std_logic; 
-             out : OUT std_logic_vector(31 DOWNTO 0));
+             MuxOut : OUT std_logic_vector(31 DOWNTO 0));
 end MUX;
 
 architecture Behavioral of MUX is
@@ -15,9 +16,9 @@ begin
 PROCESS (opt, a, b) IS
        BEGIN
          CASE opt IS
-           WHEN '0' => out <= a; 
-           WHEN '1' => out <= b;
-           WHEN OTHERS => out <= (others => '0');
+           WHEN '0' => MuxOut <= a; 
+           WHEN '1' => MuxOut <= b;
+           WHEN OTHERS => MuxOut <= (others => '0');
          END CASE;
 END PROCESS;
 
