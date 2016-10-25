01cf1ab5d718e630fe44a88e5b9464e6c530ad60
juansuva <suarez_2929@hotmail.com>
2016-10-24 20:32:41 -0500

    Carpetas

---
+++ Procesador1/InstructionMemory_tb.vhd
@@ -0,0 +1,99 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   17:31:39 10/04/2016
+-- Design Name:   
+-- Module Name:   C:/Users/utp/Desktop/ProcesadorMonociclo/ProcesadorMonociclo/InstructionMemory_tb.vhd
+-- Project Name:  ProcesadorMonociclo
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: InstructionMemory
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY InstructionMemory_tb IS
+END InstructionMemory_tb;
+ 
+ARCHITECTURE behavior OF InstructionMemory_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT InstructionMemory
+    PORT(
+         address : IN  std_logic_vector(31 downto 0);
+         reset : IN  std_logic;
+			--clkFPGA : IN  std_logic;
+         outInstruction : OUT  std_logic_vector(31 downto 0)
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal address : std_logic_vector(31 downto 0) := (others => '0');
+   signal reset : std_logic := '0';
+	--signal clkFPGA : std_logic := '0';
+	
+ 	--Outputs
+   signal outInstruction : std_logic_vector(31 downto 0);
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+  -- constant clkFPGA_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: InstructionMemory PORT MAP (
+          address => address,
+          reset => reset,
+	--		 clkFPGA => clkFPGA,
+          outInstruction => outInstruction
+        );
+
+   -- Clock process definitions
+--   clkFPGA_process :process
+--   begin
+--		clkFPGA <= '0';
+--		wait for clkFPGA_period/2;
+--		clkFPGA <= '1';
+	--	wait for clkFPGA_period/2;
+   --end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+    begin		
+      -- hold reset state for 100 ns.
+		wait for 100 ns;
+		reset<='0';
+		address <= x"00000001";
+		wait for 100 ns;
+		reset <= '1';
+      wait for 100 ns;	
+		reset<='0';
+		address <= "00000000000000000000000000011111";
+      wait;
+   end process;
+
+END;
